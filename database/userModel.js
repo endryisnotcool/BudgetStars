5b6c0202c9b48da2c37c40964364708853b69988
Yixuan Yang <127252858+yixuanyang123@users.noreply.github.com>
2024-12-06 15:10:21 -0500

    userID

--- database/userModel.js
+++ database/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
@@ -14,5 +14,3 @@ userSchema.pre('save', async function(next) {
   }
   next();
 });
-
-module.exports = mongoose.model('User', userSchema);
